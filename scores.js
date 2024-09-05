b1ffc1ab1dbb30ed33c83f1b8e888a0c6158237a
happy-holidays <154296539+happy-holidays@users.noreply.github.com>
2024-09-05 12:24:40 -0700

    Update scores.js
    add logs

--- scores.js
+++ scores.js
@@ -14,27 +14,47 @@ function main() {
     alert("Error: Invalid URL.\n\nFor reference, the URL should look like this:\nhttps://app.edulastic.com/home/class/CLASS_ID/test/TEST_ID/testActivityReport/TEST_REPORT_ID");
     return;
   }
+  
   let matches = url_regex.exec(window.location.href);
   let group_id = matches[1];
   let test_id = matches[3];
   let request_url = `https://app.edulastic.com/api/test-activity/${test_id}/report?groupId=${group_id}`;
   
   let token_list = JSON.parse(localStorage.getItem("tokens"));
-  let token = localStorage.getItem(token_list[0]);
+  let token = localStorage.getItem(token_list ? token_list[0] : null);
+
+  if (!token) {
+    alert("Error: Unable to retrieve authorization token.");
+    return;
+  }
 
   let headers = [
     ["Authorization", token]
   ];
-  http_get(request_url, function(){
+
+  http_get(request_url, function() {
     if (this.status != 200) {
       alert(`Error: Status code ${this.status} received while trying to fetch the API.`);
       return;
     }
-    let report = JSON.parse(this.responseText);
-    let wrong = report.result.testActivity.wrong;
-    let total = report.result.questionActivities.length;
-    let percent = (100 * (total - wrong) / total).toFixed(2);
-    alert(`${total - wrong}/${total} questions correct (~${percent}%)`);
+
+    let report;
+    try {
+      report = JSON.parse(this.responseText);
+    } catch (error) {
+      alert("Error: Failed to parse the response.");
+      return;
+    }
+
+    let wrong = report?.result?.testActivity?.wrong || 0;
+    let total = report?.result?.questionActivities?.length || 0;
+
+    if (total > 0) {
+      let percent = (100 * (total - wrong) / total).toFixed(2);
+      alert(`${total - wrong}/${total} questions correct (~${percent}%)`);
+    } else {
+      alert("Error: No questions found in the report.");
+    }
   }, headers);
 }
 
