5b874062aed96ab473053807e37b256718dafc67
happy-holidays <154296539+happy-holidays@users.noreply.github.com>
2024-09-05 12:19:07 -0700

    Update scores.js
    update the url so it works

--- scores.js
+++ scores.js
@@ -9,15 +9,15 @@ function http_get(url, callback, headers=[], method="GET", content=null) {
 }
 
 function main() {
-  let url_regex = /https:\/\/assessment\.peardeck\.com\/home\/class\/([a-f0-9]+)\/test\/([a-f0-9]+)\/testActivityReport\/([a-f0-9]+)/;
-  if (!url_regex.test(window.location)) {
-    alert("Error: Invalid URL.\n\nFor reference, the URL should look like this:\nhttps://assessment.peardeck.com/home/class/CLASS_ID/test/TEST_ID/testActivityReport/TEST_REPORT_ID");
+  let url_regex = /https:\/\/app\.edulastic\.com\/home\/class\/([a-f0-9]+)\/test\/([a-f0-9]+)\/testActivityReport\/([a-f0-9]+)/;
+  if (!url_regex.test(window.location.href)) {
+    alert("Error: Invalid URL.\n\nFor reference, the URL should look like this:\nhttps://app.edulastic.com/home/class/CLASS_ID/test/TEST_ID/testActivityReport/TEST_REPORT_ID");
     return;
   }
-  let matches = url_regex.exec(window.location);
+  let matches = url_regex.exec(window.location.href);
   let group_id = matches[1];
   let test_id = matches[3];
-  let request_url = `https://assessment.peardeck.com/api/test-activity/${test_id}/report?groupId=${group_id}`;
+  let request_url = `https://app.edulastic.com/api/test-activity/${test_id}/report?groupId=${group_id}`;
   
   let token_list = JSON.parse(localStorage.getItem("tokens"));
   let token = localStorage.getItem(token_list[0]);
@@ -27,14 +27,14 @@ function main() {
   ];
   http_get(request_url, function(){
     if (this.status != 200) {
-      alert(`Error: Status code ${this.status} recieved while trying to fetch the API.`);
+      alert(`Error: Status code ${this.status} received while trying to fetch the API.`);
       return;
     }
     let report = JSON.parse(this.responseText);
     let wrong = report.result.testActivity.wrong;
     let total = report.result.questionActivities.length;
-    let percent = (100*(total-wrong)/total).toFixed(2);
-    alert(`${total-wrong}/${total} questions correct (~${percent}%)`);
+    let percent = (100 * (total - wrong) / total).toFixed(2);
+    alert(`${total - wrong}/${total} questions correct (~${percent}%)`);
   }, headers);
 }
 
